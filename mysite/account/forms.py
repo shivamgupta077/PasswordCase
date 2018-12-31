6be8c8e3320a31c3fbca3f3ab48606e380e1cac5
Mayur Vaid <mayur26vaid@gmail.com>
2018-12-31 15:31:23 +0530

    New Commit

--- mysite/account/forms.py
+++ mysite/account/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 from .models import Case, Profile, Passwords
 
-class CaseForm(forms.ModelForm):
+class RegisterForm(forms.ModelForm):
     class Meta:
         model = Case
         fields = ('username', 'password', 'phone_number', 'answer1', 'answer2', 'answer3', 'answer4', 'answer5')
 
+class LoginForm(forms.ModelForm):
+    inp_username = forms.CharField(label="Username ", max_length=100)
+    inp_password = forms.CharField(label="Password ", max_length=100)
--- mysite/account/views.py
+++ mysite/account/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from .forms import CaseForm
+from .forms import RegisterForm, LoginForm
 from .models import Case, Profile, Passwords
 
 # Create your views here.
 def register(request):
     if request.method=='POST':
-        form = CaseForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             #Save input in database
             case = Case()
@@ -24,15 +24,44 @@ def register(request):
             context = {}
             return render(request, 'account/login.html', context)
         else:
-            blank_form = CaseForm()
+            blank_form = RegisterForm()
             context = {'form': blank_form}
             return render(request, 'account/register.html', context)
     else:   #if get method, create a blank form.
-        blank_form = CaseForm()
+        blank_form = RegisterForm()
         context = {'form': blank_form}
         return render(request, 'account/register.html', context)
 
 
 def login(request):
-    context = {}
-    return render(request, 'account/login.html', context)
+    if request.method=='POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            #Save input in database
+            inpusername = form.cleaned_data.get("inpusername")
+            inppassword = form.cleaned_data.get("inppassword")
+            CaseObjList = Case.objects.all()
+            ReqObj = None
+            for obj in CaseObjList:
+                if obj.username==inpusername and obj.password==inppassword:
+                    ReqObj = obj
+                    break
+            if ReqObj==None:
+                blank_form = LoginForm()
+                context = {'form': blank_form}
+                return render(request, 'account/login.html', context)
+            #
+            #
+            #Collect all data of ReqObj and send it to template.
+            #
+            #
+            context = {}
+            return render(request, 'account/profile.html', context)
+        else:
+            blank_form = LoginForm()
+            context = {'form': blank_form}
+            return render(request, 'account/login.html', context)
+    else:   #if get method, create a blank form.
+        blank_form = LoginForm()
+        context = {'form': blank_form}
+        return render(request, 'account/login.html', context)
